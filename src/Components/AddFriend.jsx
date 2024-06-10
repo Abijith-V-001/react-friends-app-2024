e3339a023d31e742fab728d0b2f5efceb7b51abd
Abijith-V-001 <abivandiyil001@gmail.com>
2024-06-10 12:47:48 +0530

    forms read

--- src/Components/AddFriend.jsx
+++ src/Components/AddFriend.jsx
@@ -1,11 +1,33 @@
 import React, { useState } from 'react'
 import Navbar from './Navbar'
+import axios from 'axios'
 
 export const AddFriend = () => {
-    
+    const [friend, changeFriend] = useState({
+        "name": "",
+        "friendName": "",
+        "friendNickName": "",
+        "DescribeYourFriend": ""
+    })
+    const inputHandler=(event)=>{
+        changeFriend({...friend,[event.target.name]:event.target.value})
+    }
+    const readValue=()=>{
+        console.log(friend)
+        axios.post("https://friendsapi-re5a.onrender.com/adddata",friend).then(
+            (response)=>{
+                console.log(response.data)
+                if (response.data.status=="success") {
+                    alert("Successfully added")
+                } else {
+                   alert("Not added") 
+                }
+            }
+        )
+    }
     return (
         <div>
-            <Navbar/>
+            <Navbar />
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 colxl-12 col-xxl-12"></div>
@@ -13,30 +35,30 @@ export const AddFriend = () => {
                         <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
                             <label htmlFor="" className="form-label">Name</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='name' value={friend.name} onChange={inputHandler}/>
 
                         </div>
                         <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
                             <label htmlFor="" className="form-label">Friend's name</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='friendName' value={friend.friendName} onChange={inputHandler}/>
 
                         </div>
                         <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
                             <label htmlFor="" className="form-label">Friend's nick name</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='friendNickName' value={friend.friendNickName} onChange={inputHandler}/>
 
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                             <label htmlFor="" className="form-label">Description</label>
-                            <textarea name="" id="" className="form-control"></textarea>
+                            <textarea name="DescribeYourFriend" id="" className="form-control" value={friend.DescribeYourFriend} onChange={inputHandler}></textarea>
 
                         </div>
                         <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
-                            <button className="btn btn-success">Add friend</button>
+                            <button className="btn btn-success" onClick={readValue}>Add friend</button>
 
                         </div>
                     </div>
--- src/Components/ViewFriends.jsx
+++ src/Components/ViewFriends.jsx
@@ -1,19 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
+import axios from 'axios'
 
 const ViewFriends = () => {
-    const [friend,changeFriend]=useState(
-        [{
-            "name": "",
-            "friendName": "",
-            "friendNickName":"",
-            "DescribeYourFriend":""
-            }]
-    )
-
+    const [friend, changeFriend] = useState([])
+    const fetchData = () => {
+        axios.get("https://friendsapi-re5a.onrender.com/view").then(
+            (response) => {
+                changeFriend(response.data)
+            }
+        ).catch().finally()
+    }
+    useEffect(() => {
+        fetchData()
+    }, [])
     return (
         <div>
-            <Navbar/>
+            <Navbar />
             <div className="container">
                 <div className="row g-3">
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
@@ -30,18 +33,17 @@ const ViewFriends = () => {
                             </thead>
                             <tbody>
                                 {
-                                friend.map(
-                                    (value,index)=>{
-                                        return <tr>
-                                    
-                                        <td>{value.name}</td>
-                                        <td>{value.friendName}</td>
-                                        <td>{value.friendNickName}</td>
-                                        <td>{value.DescribeYourFriend}</td>
-                                    </tr>
-                                    }
-                                )}
-                                
+                                    friend.map(
+                                        (value, index) => {
+                                            return <tr>
+                                                <td>{value.name}</td>
+                                                <td>{value.friendName}</td>
+                                                <td>{value.friendNickName}</td>
+                                                <td>{value.DescribeYourFriend}</td>
+                                            </tr>
+                                        }
+                                    )}
+
                             </tbody>
                         </table>
 
