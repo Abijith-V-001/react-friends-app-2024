d49f32683155c162d8a4e13c23044182b0d3a381
Abijith-V-001 <abivandiyil001@gmail.com>
2024-06-10 12:00:03 +0530

    routing done

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import { AddFriend } from './Components/AddFriend';
 import SearchFriend from './Components/SearchFriend';
 import ViewFriends from './Components/ViewFriends';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <AddFriend/>
-      <SearchFriend/>
-      <ViewFriends/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<AddFriend/>}/>
+      <Route path='/search' element={<SearchFriend/>}/>
+      <Route path='/view' element={<ViewFriends/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
